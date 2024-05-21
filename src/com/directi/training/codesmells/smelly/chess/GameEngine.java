3183c19b988dfcc28ed651044b89bc504642fb6a
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-05-21 11:03:31 +0200

    refactorizado el metodo de restaurar tabal

--- src/com/directi/training/codesmells/smelly/chess/GameEngine.java
+++ src/com/directi/training/codesmells/smelly/chess/GameEngine.java
@@ -79,29 +79,20 @@ public class GameEngine
 
     public void resetBoard()
     {
-        for (int column = 0; column < 8; column++) {
-            if (column == 0) {
-                _chessBoard.getBoard()[7][column].setPiece(new LeftRook(Color.WHITE));
-            } else if (column == 1) {
-                _chessBoard.getBoard()[7][column].setPiece(new LeftKnight(Color.WHITE));
-            } else if (column == 2) {
-                _chessBoard.getBoard()[7][column].setPiece(new LeftBishop(Color.WHITE));
-            } else if (column == 3) {
-                _chessBoard.getBoard()[7][column].setPiece(new King(Color.WHITE));
-            } else if (column == 4) {
-                _chessBoard.getBoard()[7][column].setPiece(new Queen(Color.WHITE));
-            } else if (column == 5) {
-                _chessBoard.getBoard()[7][column].setPiece(new RightBishop(Color.WHITE));
-            } else if (column == 6) {
-                _chessBoard.getBoard()[7][column].setPiece(new RightKnight(Color.WHITE));
-            } else if (column == 7) {
-                _chessBoard.getBoard()[7][column].setPiece(new RightRook(Color.WHITE));
-            }
-        }
+        recolocarWhite();
         for (int column = 0; column < 8; column++) {
             _chessBoard.getBoard()[6][column].setPiece(new Pawn(Color.WHITE));
         }
 
+        recolocarBlack();
+        for (int column = 0; column < 8; column++) {
+            _chessBoard.getBoard()[1][column].setPiece(new Pawn(Color.BLACK));
+        }
+
+        _chessBoard._kingDead = false;
+    }
+
+    private void recolocarBlack() {
         for (int column = 0; column < 8; column++) {
             if (column == 0) {
                 _chessBoard.getBoard()[0][column].setPiece(new LeftRook(Color.BLACK));
@@ -121,11 +112,28 @@ public class GameEngine
                 _chessBoard.getBoard()[0][column].setPiece(new RightRook(Color.BLACK));
             }
         }
-        for (int column = 0; column < 8; column++) {
-            _chessBoard.getBoard()[1][column].setPiece(new Pawn(Color.BLACK));
-        }
+    }
 
-        _chessBoard._kingDead = false;
+    private void recolocarWhite() {
+        for (int column = 0; column < 8; column++) {
+            if (column == 0) {
+                _chessBoard.getBoard()[7][column].setPiece(new LeftRook(Color.WHITE));
+            } else if (column == 1) {
+                _chessBoard.getBoard()[7][column].setPiece(new LeftKnight(Color.WHITE));
+            } else if (column == 2) {
+                _chessBoard.getBoard()[7][column].setPiece(new LeftBishop(Color.WHITE));
+            } else if (column == 3) {
+                _chessBoard.getBoard()[7][column].setPiece(new King(Color.WHITE));
+            } else if (column == 4) {
+                _chessBoard.getBoard()[7][column].setPiece(new Queen(Color.WHITE));
+            } else if (column == 5) {
+                _chessBoard.getBoard()[7][column].setPiece(new RightBishop(Color.WHITE));
+            } else if (column == 6) {
+                _chessBoard.getBoard()[7][column].setPiece(new RightKnight(Color.WHITE));
+            } else if (column == 7) {
+                _chessBoard.getBoard()[7][column].setPiece(new RightRook(Color.WHITE));
+            }
+        }
     }
 
     private void endGame()
